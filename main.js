f395bfefd33415841b5433d6612bd4d134392b36
NKhoauit <khoanhat2003.work@gmail.com>
2024-03-01 16:03:59 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var fullName = 'Nguyen Ho Nhat Khoa';
+var age = 26;
+// alert(fullName);
+// alert(age);
+
+// console.error(fullName);
+
+// confirm('Xac nhan ban du 18 tuoi');
+// prompt('Xac nhan du 18 tuoi');
+// setTimeout(function() { alert('Thong bao')
+// },1000)
+// setInterval (function(){console.log('Day la log')}
+// ,1000)
