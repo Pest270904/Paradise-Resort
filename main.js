cf41b0ab3784e1ac1ffde37c5ddf17e4d1bfb131
NKhoauit <khoanhat2003.work@gmail.com>
2024-03-17 19:30:58 +0700

    update login box

--- main.js
+++ main.js
@@ -2,3 +2,20 @@ const chatboxToggler = document.querySelector(".chatbox-toggler")
 
 chatboxToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbox"));
 // test commit
+
+// srcipt register/login box
+
+const login_display = document.querySelector('.modal');
+const btn__login = document.querySelector('.btn-login');
+const modal_container = document.querySelector('.modal__body')
+// console.log(modal_container)
+// console.log(btn__login);
+btn__login.addEventListener("click",function(){
+    login_display.classList.add('open__btn');
+})
+login_display.addEventListener('click',function(){
+    login_display.classList.remove('open__btn')
+})
+modal_container.addEventListener('click', function (event) {
+    event.stopPropagation();
+})
