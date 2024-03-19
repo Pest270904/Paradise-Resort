6558da3a36cbe3d3127d062dbcc3bec8e51dbf45
tnh47 <115148368+tnh47@users.noreply.github.com>
2024-03-19 14:32:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+// script chatbox
 const chatboxToggler = document.querySelector(".chatbox-toggler")
-
 chatboxToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbox"));
-// test commit
+
 
 // srcipt register/login box
 
