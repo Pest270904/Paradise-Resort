4a72130bba050e0d777c8b8aff09f9a750c451ef
Pest270904 <ntpllc2004@gmail.com>
2024-05-18 17:27:46 +0700

    final vercel fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
@@ -67,7 +67,7 @@ export class AppModule implements NestModule{
           ReviewController,
           PaymentController
         )
-        .apply(CheckLoginMiddleware).forRoutes(
+        .apply(CheckLoginMiddleware).exclude({path:'/admin/create', method: RequestMethod.GET}).forRoutes(
           ReservationController,
           UserController,
           AdminController
