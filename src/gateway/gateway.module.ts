fbbf2efbc0ef769c40327bd708a58f98a7c3e0d9
Pest270904 <ntpllc2004@gmail.com>
2024-05-19 17:55:06 +0700

    test

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { AdminModule } from "src/admin/admin.module";
 import { AdminService } from "src/admin/admin.service";
 import { FuncService } from "src/func/func.service";
-import { Gateway } from "./gateway";
 
 @Module({
     imports: [AdminModule],
-    providers: [FuncService, AdminService, Gateway]
+    providers: [FuncService, AdminService]
 })
 export class GatewayModule{}
